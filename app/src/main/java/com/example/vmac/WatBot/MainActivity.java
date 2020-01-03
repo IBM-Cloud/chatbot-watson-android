97f8fcaef372dc1598484ce4bb027d7c745a922e
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2020-01-03 16:25:27 +0530

    Added refresh button

--- app/src/main/java/com/example/vmac/WatBot/MainActivity.java
+++ app/src/main/java/com/example/vmac/WatBot/MainActivity.java
@@ -16,6 +16,8 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -157,9 +159,25 @@ public class MainActivity extends AppCompatActivity {
 
         createServices();
         sendMessage();
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 
-    ;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { switch(item.getItemId()) {
+        case R.id.refresh:
+            finish();
+            startActivity(getIntent());
+
+    }
+        return(super.onOptionsItemSelected(item));
+    }
+
 
     // Speech-to-Text Record Audio permission
     @Override
